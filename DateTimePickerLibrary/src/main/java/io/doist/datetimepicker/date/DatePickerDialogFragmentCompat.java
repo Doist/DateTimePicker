7b5feffacdc493fd7e81b92b09acb71edb64a0ff
João Martins Costa <jpmcosta@gmail.com>
2015-04-29 12:56:39 +0100

    API 22 date and time pickers, ported to 16+ APIs
    Supports a DialogFragment, instead of a Dialog.

---
+++ DateTimePickerLibrary/src/main/java/io/doist/datetimepicker/date/DatePickerDialogFragmentCompat.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.doist.datetimepicker.date;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import io.doist.datetimepicker.R;
+
+/**
+ * Dialog allowing users to select a date.
+ */
+public class DatePickerDialogFragmentCompat extends DialogFragment implements DatePicker.OnDateChangedListener {
+    public static final String TAG = DatePickerDialogFragmentCompat.class.getName();
+
+    private static final String KEY_YEAR = "year";
+    private static final String KEY_MONTH_OF_YEAR = "month";
+    private static final String KEY_DAY_OF_MONTH = "day";
+
+    private DatePicker mDatePicker;
+
+    private OnDateSetListener mOnDateSetListener;
+
+    public DatePickerDialogFragmentCompat() { }
+
+    public static DatePickerDialogFragmentCompat newInstance(OnDateSetListener listener, int year, int monthOfYear,
+                                                             int dayOfMonth) {
+        DatePickerDialogFragmentCompat fragment = new DatePickerDialogFragmentCompat();
+        Bundle arguments = new Bundle();
+        arguments.putInt(KEY_YEAR, year);
+        arguments.putInt(KEY_MONTH_OF_YEAR, monthOfYear);
+        arguments.putInt(KEY_DAY_OF_MONTH, dayOfMonth);
+        fragment.setArguments(arguments);
+        fragment.setOnDateSetListener(listener);
+        return fragment;
+    }
+
+    public void setOnDateSetListener(OnDateSetListener listener) {
+        mOnDateSetListener = listener;
+    }
+
+    static int resolveDialogTheme(Context context, int resid) {
+        if (resid == 0) {
+            final TypedValue outValue = new TypedValue();
+            context.getTheme().resolveAttribute(R.attr.datePickerDialogTheme, outValue, true);
+            return outValue.resourceId;
+        } else {
+            return resid;
+        }
+    }
+
+    @SuppressWarnings("InflateParams")
+    @NonNull
+    @Override
+    public final Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.date_picker_dialog, null);
+        mDatePicker = (DatePicker) view.findViewById(R.id.datePicker);
+        if (savedInstanceState == null) {
+            Bundle arguments = getArguments();
+            int year = arguments.getInt(KEY_YEAR);
+            int monthOfYear = arguments.getInt(KEY_MONTH_OF_YEAR);
+            int dayOfMonth = arguments.getInt(KEY_DAY_OF_MONTH);
+            mDatePicker.init(year, monthOfYear, dayOfMonth, this);
+        } else {
+            mDatePicker.setOnDateChangedListener(this);
+        }
+
+        final AlertDialog dialog = onCreateDialogBuilder(view).create();
+
+        mDatePicker.setValidationCallback(new DatePicker.ValidationCallback() {
+            @Override
+            public void onValidationChanged(boolean valid) {
+                final Button positive = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                if (positive != null) {
+                    positive.setEnabled(valid);
+                }
+            }
+        });
+
+        return dialog;
+    }
+
+    /**
+     * Allows sub-classes to customize AlertDialog.
+     */
+    protected AlertDialog.Builder onCreateDialogBuilder(View view) {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity(), resolveDialogTheme(getActivity(), getTheme()));
+        builder.setView(view);
+        builder.setPositiveButton(R.string.done_label, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mOnDateSetListener != null) {
+                    mOnDateSetListener.onDateSet(
+                            mDatePicker,
+                            mDatePicker.getYear(),
+                            mDatePicker.getMonth(),
+                            mDatePicker.getDayOfMonth());
+                }
+            }
+        });
+        // Make sure the dialog width works as WRAP_CONTENT.
+        builder.getContext().getTheme().applyStyle(R.style.Theme_Window_NoMinWidth, true);
+        return builder;
+    }
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int month, int day) {
+        mDatePicker.init(year, month, day, this);
+    }
+
+    /**
+     * Gets the {@link DatePicker} contained in this dialog.
+     *
+     * @return The calendar view.
+     */
+    public DatePicker getDatePicker() {
+        return mDatePicker;
+    }
+
+    /**
+     * Sets the current date.
+     *
+     * @param year The date year.
+     * @param monthOfYear The date month.
+     * @param dayOfMonth The date day of month.
+     */
+    public void updateDate(int year, int monthOfYear, int dayOfMonth) {
+        mDatePicker.updateDate(year, monthOfYear, dayOfMonth);
+    }
+}
---
+++ DateTimePickerSamples/src/main/java/io/doist/datetimepicker/sample/MainActivity.java
@@ -0,0 +1,97 @@
+package io.doist.datetimepicker.sample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import io.doist.datetimepicker.date.DatePicker;
+import io.doist.datetimepicker.date.DatePickerDialogFragmentCompat;
+import io.doist.datetimepicker.date.OnDateSetListener;
+import io.doist.datetimepicker.time.OnTimeSetListener;
+import io.doist.datetimepicker.time.TimePicker;
+import io.doist.datetimepicker.time.TimePickerDialogFragmentCompat;
+
+
+public class MainActivity extends AppCompatActivity implements OnDateSetListener, OnTimeSetListener {
+    private Calendar mCalendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        DatePickerDialogFragmentCompat datePickerDialog =
+                (DatePickerDialogFragmentCompat) getSupportFragmentManager().findFragmentByTag(
+                        DatePickerDialogFragmentCompat.TAG);
+        if (datePickerDialog != null) {
+            datePickerDialog.setOnDateSetListener(this);
+        }
+
+        TimePickerDialogFragmentCompat timePickerDialog =
+                (TimePickerDialogFragmentCompat) getSupportFragmentManager().findFragmentByTag(
+                        TimePickerDialogFragmentCompat.TAG);
+        if (timePickerDialog != null) {
+            timePickerDialog.setOnTimeSetListener(this);
+        }
+    }
+
+    public void showDatePicker(View v) {
+        DatePickerDialogFragmentCompat.newInstance(
+                this,
+                mCalendar.get(Calendar.YEAR),
+                mCalendar.get(Calendar.MONTH),
+                mCalendar.get(Calendar.DAY_OF_MONTH)).show(getSupportFragmentManager(), DatePickerDialogFragmentCompat.TAG);
+    }
+
+    public void showTimePicker(View v) {
+        TimePickerDialogFragmentCompat.newInstance(
+                this,
+                mCalendar.get(Calendar.HOUR_OF_DAY),
+                mCalendar.get(Calendar.MINUTE),
+                false).show(getSupportFragmentManager(), TimePickerDialogFragmentCompat.TAG);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Toast.makeText(this, year + "/" + (monthOfYear + 1) + "/" + dayOfMonth, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(this, hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+    }
+}
